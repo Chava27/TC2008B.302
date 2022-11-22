544efe708db69f6e10547aa9813b7303341b4bd1
Stephan Guingor <stephan.guingor04@gmail.com>
2022-11-22 14:22:23 -0600

    fix master

--- Act_Int/flask_server/mesa_model/agent.py
+++ Act_Int/flask_server/mesa_model/agent.py
@@ -66,8 +66,6 @@ class RobotAgent(SerializeAgent):
 
     def explore(self):
         #Reduce vision intensity if the robots gets to n steps without finding any box
-        if self.explore_steps%50 == 0 and self.vision_intensity>1:
-            self.vision_intensity -=1
         possible_steps = self.model.grid.get_neighborhood(
             self.pos,
             moore=False) # Boolean for whether to use Moore neighborhood (including diagonals) or Von Neumann (only up/down/left/right)
@@ -84,60 +82,63 @@ class RobotAgent(SerializeAgent):
             self.model.grid.move_agent(self, (self.pos[0]-1, self.pos[1]))
         self.explore_steps += 1
 
+ 
     def move_to_closest_box(self) ->  int:
-    
+        print("Vision Intensity",self.vision_intensity)
         #UP, DOWN, RIGHT, LEFT
         score=[self.vision_intensity for i in range(4)]
         for y in range(self.pos[1]+1, self.pos[1]+self.vision_intensity+1):
             pos = (self.pos[0],y)
+            #Modificar self.model.grid_is_cell_empty(pos) <------
             if self.model.grid.is_cell_empty(pos):
                 score[DirectionsEnum.UP]+=1
                 continue
 
             instance = self.model.grid.get_cell_list_contents(pos)[0]
             if isinstance(instance,BoxAgent) and not instance.picked:
-                score[DirectionsEnum.UP]+= (y-self.pos[1]+1)*2
+                score[DirectionsEnum.UP]+= (self.vision_intensity-abs(y-self.pos[1])+1)*self.vision_intensity
                 break
             break
         for y in range(self.pos[1]-1, self.pos[1]-self.vision_intensity-1,-1):
             pos = (self.pos[0],y)
-            if self.model.grid.is_cell_empty(pos):
+            if  self.model.grid.is_cell_empty(pos):
                 score[DirectionsEnum.DOWN]+=1
                 continue
             
             instance = self.model.grid.get_cell_list_contents(pos)[0]
             if isinstance(instance,BoxAgent) and not instance.picked:
-                score[DirectionsEnum.DOWN]+= (self.pos[1]-y+1)*2
+                score[DirectionsEnum.DOWN]+= (self.vision_intensity-abs(self.pos[1]-y)+1)*self.vision_intensity
                 break
             break
         for x in range(self.pos[0]+1, self.pos[0]+self.vision_intensity+1):
             pos = (x,self.pos[1])
-            if self.model.grid.is_cell_empty(pos):
+            if  self.model.grid.is_cell_empty(pos):
                 score[DirectionsEnum.RIGHT]+=1
                 continue
 
             instance = self.model.grid.get_cell_list_contents(pos)[0]
             if isinstance(instance,BoxAgent) and not instance.picked:
-                score[DirectionsEnum.RIGHT]+= (x-self.pos[0]+1)*2
+                score[DirectionsEnum.RIGHT]+= (self.vision_intensity-abs(x-self.pos[0])+1)*self.vision_intensity
                 break
             break
         for x in range(self.pos[0]-1, self.pos[0]-self.vision_intensity-1,-1):
             pos = (x,self.pos[1])
-            if self.model.grid.is_cell_empty(pos):
+            if  self.model.grid.is_cell_empty(pos):
                 score[DirectionsEnum.LEFT]+=1
                 continue
-            
             instance = self.model.grid.get_cell_list_contents(pos)[0]
             if isinstance(instance,BoxAgent) and not instance.picked:
-                score[DirectionsEnum.LEFT]+= (self.pos[0]-x+1)*2
+                score[DirectionsEnum.LEFT]+= (self.vision_intensity-abs(self.pos[0]-x)+1)*self.vision_intensity
                 break
             break
         scores= list(filter(lambda x: x!=0, score)) 
+        print("Scores:",scores)
         if len(scores)==0:
             return DirectionsEnum.CENTER
         direction = random.choice([i for i, x in enumerate(score) if x == max(score)])
-        return direction
-                   
+        print("Direction:",direction)
+        return direction        
+
     def pickup_closest_box(self,possible_steps) -> bool:
         for step in possible_steps:
             cell_container= self.model.grid.get_cell_list_contents(step)
